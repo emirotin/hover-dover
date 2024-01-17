824b7dc62080e00ae3fed45cffd2df29f5cc5cc1
Eugene Mirotin <emirotin@gmail.com>
2024-01-17 20:50:44 +0200

    fix astro folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	base: "/hover-dover",
+	build: {
+		assets: "astro",
+	},
 	vite: {
 		ssr: {
 			noExternal: ["modern-normalize"],
